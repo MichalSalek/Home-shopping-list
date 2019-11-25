134a828477ae74c36c29ff2ab1930e3fd91370b8
MichalSalek <michal.salek@abventor.com>
2019-11-25 23:55:41 +0100

    Added Prettier and its config

--- src/components/enter-new-product.tsx
+++ src/components/enter-new-product.tsx
@@ -1,22 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import {OwnInput} from "../atoms/own-input";
-import {OwnButton} from "../atoms/own-button";
+import { OwnInput } from '../atoms/own-input';
+import { OwnButton } from '../atoms/own-button';
 // Material
-import {Container} from "@material-ui/core";
+import { Container } from '@material-ui/core';
 
 export const EnterNewProduct = () => {
-    const [newProduct, setNewProduct] = useState("");
+  const [newProduct, setNewProduct] = useState('');
 
-    const addNewProduct = (): void => {
-        const value = newProduct;
-        setNewProduct("")
-    };
+  const addNewProduct = (): void => {
+    const value = newProduct;
+    setNewProduct('');
+  };
 
-    return (
-        <Container fixed>
-            <OwnInput id="enter-new-product" label="Czego brakuje?" setter={setNewProduct} value={newProduct}/>
-            <OwnButton clicker={addNewProduct} textContent="Dodaj produkt" loading={true} />
-        </Container>
-    )
+  return (
+    <Container fixed>
+      <OwnInput
+        id="enter-new-product"
+        label="Czego brakuje?"
+        setter={setNewProduct}
+        value={newProduct}
+      />
+      <OwnButton
+        clicker={addNewProduct}
+        textContent="Dodaj produkt"
+        loading={true}
+      />
+    </Container>
+  );
 };
